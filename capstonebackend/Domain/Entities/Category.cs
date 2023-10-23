5473fbfc0bd5736b2a36589f94487d516fbf0d65
Nguyen Thanh Tung <tungtn0727@gmail.com>
2023-10-23 13:37:07 +0700

    first commit(api)

---
+++ capstonebackend/Domain/Entities/Category.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+namespace capstonebackend.Domain.Entities
+{
+    public partial class Category
+    {
+        public Guid Id {get;set;}
+    }
+}
