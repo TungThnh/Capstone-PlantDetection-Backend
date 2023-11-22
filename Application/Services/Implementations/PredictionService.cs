26bfb6a9596bb50c5f1654b3a32699be581fc071
Nguyen Thanh Tung <tungtn0727@gmail.com>
2023-11-22 22:37:13 +0700

    Update PredictionService.cs

--- Application/Services/Implementations/PredictionService.cs
+++ Application/Services/Implementations/PredictionService.cs
@@ -1,8 +1,10 @@
 ﻿using Application.Services.Interfaces;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
+using Common.Constants;
 using Data;
 using Data.Repositories.Interfaces;
+using Domain.Models.Creates;
 using Domain.Models.Systems;
 using Domain.Models.Views;
 using Microsoft.AspNetCore.Http;
@@ -15,19 +17,21 @@ using System.Text.RegularExpressions;
 
 namespace Application.Services.Implementations
 {
-    public class PredictionService: BaseService, IPredictionService
+    public class PredictionService : BaseService, IPredictionService
     {
         private readonly ICloudStorageService _cloudStorageService;
         private readonly IPlantRepository _plantRepository;
+        private readonly IClassRepository _classRepository;
         private readonly new IMapper _mapper;
 
         private const double MinimumRate = 0.7;
 
         public PredictionService(IUnitOfWork unitOfWork, IMapper mapper, ICloudStorageService cloudStorageService) : base(unitOfWork, mapper)
         {
-           _cloudStorageService = cloudStorageService;
+            _cloudStorageService = cloudStorageService;
             _mapper = mapper;
             _plantRepository = unitOfWork.Plant;
+            _classRepository = unitOfWork.Class;
         }
 
         private async Task<string> ExecuteCommandAsync(string workingDirectory, string command, string arguments)
@@ -85,7 +89,8 @@ namespace Application.Services.Implementations
             }
         }
 
-        public List<TerminalResult> GetResult(string terminalOutput) {
+        public List<TerminalResult> GetResult(string terminalOutput)
+        {
 
             // Sử dụng regular expressions để trích xuất các giá trị
             string pattern = @"(?<label>\w+) (?<confidence>[\d.]+),";
@@ -147,6 +152,59 @@ namespace Application.Services.Implementations
             }
         }
 
+        public async Task<IActionResult> ProgressImageForClass(Guid classId, IFormFile image)
+        {
+            try
+            {
+                var imageId = Guid.NewGuid();
+                var classCode = _classRepository.GetMany(cl => cl.Id.Equals(classId)).Select(cl => cl.Code).FirstOrDefault();
+
+                if (classCode == null)
+                {
+                    return new ObjectResult(CustomErrors.RecordNotFound)
+                    {
+                        StatusCode = StatusCodes.Status404NotFound
+                    };
+                }
+
+                string workingDirectory = @"C:\Users\Janglee\Desktop\plant_classfify";
+                string classDirectory = workingDirectory + "\\" + classCode;
+                var imageUrl = await GetImageUrl(imageId, image);
+                string command = "py";
+                string arguments = "main.py " + imageUrl + " " + classDirectory;
+
+                var output = await ExecuteCommandAsync(workingDirectory, command, arguments);
+
+                var terminalResult = GetResult(output);
+
+                var estimate = await GetEstimateResult(terminalResult);
+
+                var finalData = new ResultViewModel
+                {
+                    CreateAt = DateTime.Now,
+                    Id = Guid.NewGuid(),
+                    Estimate = estimate,
+                };
+
+                var bestResult = terminalResult[0];
+
+                if (bestResult != null && bestResult.Confidence > MinimumRate)
+                {
+                    var plant = await GetPlant(bestResult.Label);
+                    finalData.Plant = plant;
+                }
+
+                return new ObjectResult(finalData)
+                {
+                    StatusCode = StatusCodes.Status200OK
+                };
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
         private async Task<PlantViewModel> GetPlant(string code)
         {
             try
@@ -167,7 +225,8 @@ namespace Application.Services.Implementations
             try
             {
                 List<EstimateViewModel> estimates = new List<EstimateViewModel>();
-                foreach (var result in results) { 
+                foreach (var result in results)
+                {
                     var plant = await _plantRepository.GetMany(pl => pl.Code.Equals(result.Label))
                         .ProjectTo<PlantViewModel>(_mapper.ConfigurationProvider)
                         .FirstOrDefaultAsync();
@@ -189,5 +248,46 @@ namespace Application.Services.Implementations
             }
         }
 
+        public IActionResult UpdateClassModel(Guid classId, ClassModelCreateModel model)
+        {
+            try
+            {
+                var file = model.Model;
+                var classCode = _classRepository.GetMany(cl => cl.Id.Equals(classId)).Select(cl => cl.Code).FirstOrDefault();
+
+                if (classCode == null)
+                {
+                    return new ObjectResult(CustomErrors.RecordNotFound)
+                    {
+                        StatusCode = StatusCodes.Status404NotFound
+                    };
+                }
+
+                string folderPath = @"C:\Users\Janglee\Desktop\plant_classfify\classes\" + classCode;
+
+                if (!Directory.Exists(folderPath))
+                {
+                    Directory.CreateDirectory(folderPath);
+                }
+
+                string filePath = Path.Combine(folderPath, file.FileName);
+
+                // Ghi file vào đường dẫn đã tạo
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+
+                return new ObjectResult("File saved successfully")
+                {
+                    StatusCode = StatusCodes.Status200OK
+                };
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
     }
 }
