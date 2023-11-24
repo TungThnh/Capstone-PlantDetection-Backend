90524edd00698fee4848e1ba12978f37e1798e65
Nguyen Thanh Tung <tungtn0727@gmail.com>
2023-11-24 15:33:15 +0700

    Update Class.cs

--- Domain/Entities/Class.cs
+++ Domain/Entities/Class.cs
@@ -9,19 +9,23 @@ public partial class Class
 
     public Guid ManagerId { get; set; }
 
-    public string Name { get; set; } = null!;
-
     public string Code { get; set; } = null!;
 
-    public string? Description { get; set; }
+    public string Name { get; set; } = null!;
 
-    public string? Note { get; set; }
+    public string? Description { get; set; }
 
     public DateTime CreateAt { get; set; }
 
     public int NumberOfMember { get; set; }
 
     public string Status { get; set; } = null!;
+
+    public string? Note { get; set; }
+
+    public string ThumbnailUrl { get; set; } = null!;
+
+    public virtual ICollection<ClassLabel> ClassLabels { get; set; } = new List<ClassLabel>();
 
     public virtual Manager Manager { get; set; } = null!;
 
